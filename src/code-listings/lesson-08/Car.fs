3bee55b4bdc9063923fc0d413b5a10fef17ce322
Brian Snyder <bsnyder@technology4retailers.com>
2018-11-07 16:42:52 -0600

    finished

--- src/code-listings/lesson-08/Car.fs
+++ src/code-listings/lesson-08/Car.fs
@@ -2,7 +2,24 @@
 
 open System
 
-//TODO: Create helper functions to provide the building blocks to implement driveTo.
+let getDistance (destination) =
+    if destination = "Gas" then 10
+    elif destination = "Home" then 25
+    elif destination = "Office" then 50
+    elif destination = "Stadium" then 25
+    else failwith "Unknown destination!"
+
+let calculateRemainingPetrol (currentPetrol:int, distance:int) = 
+    if distance < currentPetrol then
+        currentPetrol - distance
+    else
+        failwith "Oops!  You've run out of petrol"
 
 /// Drives to a given destination given a starting amount of petrol
-let driveTo (petrol, destination) = petrol
+let driveTo (petrol, destination) =
+    let petrol = calculateRemainingPetrol(petrol, getDistance(destination))
+    
+    if destination = "Gas" then 
+        petrol + 50
+    else
+        petrol
